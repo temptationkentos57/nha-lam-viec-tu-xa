9cec4953586be986d9ff0d8b622c70199585c6d4
temptationkentos57 <kreetlannoo@hotmail.com>
2025-09-01 17:37:00 +0200

    feat: initial commit for models.js

---
+++ models.js
@@ -0,0 +1,9 @@
+class Project {
+    constructor(id, title, freelancerId) {
+        this.id = id;
+        this.title = title;
+        this.freelancerId = freelancerId;
+    }
+}
+
+module.exports = { Project };
